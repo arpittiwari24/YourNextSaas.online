fc71cb239be167ef195da0eb89312ba4c62a65f2
arpittiwari24 <atofficial2410@gmail.com>
2024-03-24 23:12:49 +0530

    made auth server side

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,19 +6,12 @@ import { useEffect, useState } from "react"
 
 const Page = () => {
     const {data: session} = useSession()
-    const [loading, setLoading] = useState<boolean>(false)
-    if(loading) {
-        return (
-            <p>Loading...</p>
-        )
-    }
+  
     useEffect(() => {
         const fetch = () => {
-            setLoading(true)
             if(!session) {
                 redirect("/")
             }
-            setLoading(false)
         }
         fetch()
     },[session])
