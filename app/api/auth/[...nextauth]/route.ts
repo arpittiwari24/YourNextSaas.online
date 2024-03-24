a4316439eda1aee69c6bf6ac2bbdf8c75fe983e4
arpittiwari24 <atofficial2410@gmail.com>
2024-03-24 23:15:01 +0530

    made auth server side

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,10 @@
+import authOptions from "@/utils/authOptions";
 import { SupabaseAdapter } from "@auth/supabase-adapter";
 import { Adapter } from "next-auth/adapters";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    }),
-  ],
-  adapter: SupabaseAdapter({
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
-    secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? "",
-  }) as Adapter,
-}
+
 
 const handler = NextAuth(authOptions);
 
---
+++ utils/authOptions.ts
@@ -0,0 +1,19 @@
+import { SupabaseAdapter } from "@auth/supabase-adapter";
+import { Adapter } from "next-auth/adapters";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+const authOptions = {
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      }),
+    ],
+    adapter: SupabaseAdapter({
+      url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+      secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? "",
+    }) as Adapter,
+  }
+
+  export default authOptions
