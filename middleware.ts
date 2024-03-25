27f57f0509b1548f09cd8e53ebefe25ef871d2d3
arpittiwari24 <atofficial2410@gmail.com>
2024-03-25 14:24:21 +0530

    added middleware

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,6 @@ export default function middleware(req: NextRequest) {
     const verify = req.cookies.get("next-auth.session-token")
     const url = req.url
     if(!verify && url.includes("/dashboard")){
-        return NextResponse.redirect("http://localhost:3000")
+        return NextResponse.redirect(new URL("/",url))
     }
 }
