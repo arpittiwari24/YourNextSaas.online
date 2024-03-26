1fb9a3e079db99be88ed3423d9e6f2df6e8aba60
arpittiwari24 <atofficial2410@gmail.com>
2024-03-26 11:55:08 +0530

    improved middleware

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest) {
     const verify = req.cookies.get("next-auth.session-token")
-    const environment = process.env.NODE_ENV
     const url = req.url
     if(!verify && url.includes("/dashboard")){
-        if(environment === "development") {
-            return NextResponse.redirect("http://localhost:3000")
-        } else {
-            return NextResponse.redirect("https://yournextsass.vercel.app")
-        }
+        return NextResponse.redirect("https://yournextsass.vercel.app")
     }
 }
