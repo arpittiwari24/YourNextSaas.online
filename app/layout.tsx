9aaba93f010a287aa4eeab092da0b5d31e12d8bb
sezallagwal <sezal1470@gmail.com>
2024-10-23 18:54:28 +0530

    added chatbot for enhanced user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,24 +4,29 @@ import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import Providers from "@/components/Providers";
 import PlausibleProvider from "next-plausible";
+import Script from "next/script";
 
 const sans = Open_Sans({ subsets: ["latin"] });
 
 // Fallback URL logic
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://yournextsass-demo.vercel.app/';
+const baseUrl =
+  process.env.NEXT_PUBLIC_BASE_URL || "https://yournextsass-demo.vercel.app/";
 
 export const metadata: Metadata = {
   title: "Your Next SaaS",
-  description: "Boost your speed while building your next SaaS with our open-sourced boilerplate. Spend your time building new features without worrying about payments, authentication, mailing, blogs, admin access, and SEO.",
-  keywords: "SaaS, Next.js, payments, authentication, mailing, blogs, admin access, SEO, open-source, web development",
+  description:
+    "Boost your speed while building your next SaaS with our open-sourced boilerplate. Spend your time building new features without worrying about payments, authentication, mailing, blogs, admin access, and SEO.",
+  keywords:
+    "SaaS, Next.js, payments, authentication, mailing, blogs, admin access, SEO, open-source, web development",
   openGraph: {
     title: "Your Next SaaS",
-    description: "Accelerate your SaaS development with our open-source boilerplate, featuring built-in solutions for payments, authentication, mailing, and more.",
+    description:
+      "Accelerate your SaaS development with our open-source boilerplate, featuring built-in solutions for payments, authentication, mailing, and more.",
     url: `${baseUrl}`,
     siteName: "Your Next SaaS",
     images: [
       {
-        url: `${baseUrl}/public/logo.png`,  
+        url: `${baseUrl}/public/logo.png`,
         width: 1200,
         height: 630,
         alt: "Your Next SaaS Open Graph Image",
@@ -30,19 +35,20 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    site: "@your_twitter_handle",  
+    site: "@your_twitter_handle",
     title: "Your Next SaaS",
-    description: "Open-source Next.js boilerplate for rapid SaaS development with pre-built solutions.",
-    images: [`${baseUrl}/public/og-image.png`],  
+    description:
+      "Open-source Next.js boilerplate for rapid SaaS development with pre-built solutions.",
+    images: [`${baseUrl}/public/og-image.png`],
   },
   alternates: {
-    canonical: `${baseUrl}`, 
+    canonical: `${baseUrl}`,
   },
   other: {
     "application-name": "Your Next SaaS",
     "msapplication-TileColor": "#ffffff",
-    "msapplication-config": "/browserconfig.xml", 
-    "theme-color": "#ffffff",  
+    "msapplication-config": "/browserconfig.xml",
+    "theme-color": "#ffffff",
   },
 };
 
@@ -55,13 +61,35 @@ export default function RootLayout({
     <html lang="en">
       {/* <script defer data-domain="" src="https://plausible.io/js/script.js"></script> */}
       <head>
-        <PlausibleProvider domain="yournextsass.vercel.app" taggedEvents={true} />
+        <PlausibleProvider
+          domain="yournextsass.vercel.app"
+          taggedEvents={true}
+        />
       </head>
       <body className={sans.className}>
         <Toaster position="top-center" />
         <Providers>
           {children}
+          
         </Providers>
+        <Script
+            id="chatbase-config"
+            strategy="beforeInteractive" // Load config before interaction
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.embeddedChatbotConfig = {
+                chatbotId: "RShRPqdFzXMb54uljd5RX",
+                domain: "www.chatbase.co"
+                };
+                `,
+            }}
+          />
+          <Script id="chatbase-script"
+          src="https://www.chatbase.co/embed.min.js"
+          chatbotId="RShRPqdFzXMb54uljd5RX"
+          domain="www.chatbase.co"
+          defer
+          />
       </body>
     </html>
   );
